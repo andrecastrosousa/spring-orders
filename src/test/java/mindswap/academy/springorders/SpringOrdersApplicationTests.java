4a7412ed38ad060a46c5b3d9de94c32e2532721d
André Sousa <andre.castro.sousa@gmail.com>
2023-08-22 13:29:29 +0000

    add: initial project

---
+++ src/test/java/mindswap/academy/springorders/SpringOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package mindswap.academy.springorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
