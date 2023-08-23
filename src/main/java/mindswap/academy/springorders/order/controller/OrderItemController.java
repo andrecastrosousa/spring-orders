fef8f65191c58ccab1fc6219af061a052b7bed81
André Sousa <andre.castro.sousa@gmail.com>
2023-08-23 00:02:45 +0000

    add: order item controller

--- src/main/java/mindswap/academy/springorders/order/controller/OrderItemController.java
+++ src/main/java/mindswap/academy/springorders/order/controller/OrderItemController.java
@@ -1,7 +1,52 @@
 package mindswap.academy.springorders.order.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.transaction.Transactional;
+import mindswap.academy.springorders.order.dto.OrderDto;
+import mindswap.academy.springorders.order.dto.OrderItemDto;
+import mindswap.academy.springorders.order.dto.OrderItemUpdateDto;
+import mindswap.academy.springorders.order.service.OrderItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController(value = "/orders")
 public class OrderItemController {
+    @Autowired
+    OrderItemService orderItemService;
+
+    @GetMapping("/{orderId}/items")
+    @RolesAllowed("user")
+    public List<OrderItemDto> get(@RequestParam("orderId") Long orderId) {
+        return orderItemService.getListOfOrderItem(orderId);
+    }
+
+    @PutMapping("/{orderId}/items")
+    @Transactional
+    public OrderDto addItemToOrder(
+            @RequestParam("orderId") Long orderId,
+            OrderItemDto orderItemAddDto
+    ) {
+        return orderItemService.addItemToOrder(orderId, orderItemAddDto);
+    }
+
+    @PutMapping("/{orderId}/items/{itemId}")
+    @Transactional
+    public OrderDto updateItemOnOrder(
+            @RequestParam("orderId") Long orderId,
+            @RequestParam("itemId") Long itemId,
+            OrderItemUpdateDto orderItemUpdateDto
+    ) {
+        return orderItemService.updateItemOnOrder(orderId, itemId, orderItemUpdateDto);
+    }
+
+    @DeleteMapping("/{orderId}/items/{itemId}")
+    @RolesAllowed("user")
+    public void removeItemFromOrder(
+            @RequestParam("orderId") Long orderId,
+            @RequestParam("itemId") Long itemId
+    ) {
+        orderItemService.removeItemFromOrder(orderId, itemId);
+    }
 }
--- src/main/java/mindswap/academy/springorders/order/service/OrderServiceImpl.java
+++ src/main/java/mindswap/academy/springorders/order/service/OrderServiceImpl.java
@@ -1,30 +1,58 @@
 package mindswap.academy.springorders.order.service;
 
+import mindswap.academy.springorders.item.dto.ItemDto;
+import mindswap.academy.springorders.order.converter.OrderConverter;
 import mindswap.academy.springorders.order.dto.OrderCreateDto;
 import mindswap.academy.springorders.order.dto.OrderDto;
+import mindswap.academy.springorders.order.model.Order;
+import mindswap.academy.springorders.order.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    OrderRepository orderRepository;
+
+
+    @Autowired
+    OrderConverter orderConverter;
+
     @Override
     public List<OrderDto> listAll(String email) {
-        return null;
+        List<OrderDto> orderDtoList = new ArrayList<>();
+        orderRepository.findAll().forEach(order -> orderDtoList.add(orderConverter.toDto(order)));
+        return orderDtoList;
     }
 
     @Override
     public OrderDto findById(String email, Long orderId) {
-        return null;
+        Order order = orderRepository.findById(orderId).orElse(null);
+        if (order == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Order not found");
+        }
+        return orderConverter.toDto(order);
     }
 
     @Override
-    public OrderDto create(String email, OrderCreateDto order) {
-        return null;
+    public OrderDto create(String email, OrderCreateDto orderCreateDto) {
+        Order order = orderConverter.toEntityFromCreateDto(orderCreateDto);
+        orderRepository.save(order);
+
+        return orderConverter.toDto(order);
     }
 
     @Override
     public void delete(String email, Long orderId) {
-
+        Order order = orderRepository.findById(orderId).orElse(null);
+        if (order == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Order not found");
+        }
+        orderRepository.delete(order);
     }
 }
